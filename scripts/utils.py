2facc3616a232b6d83be23b1df3d21355adcd860
mille055 <46400739+mille055@users.noreply.github.com>
2023-04-05 10:33:06 -0400

    changed path to conifg

--- scripts/utils.py
+++ scripts/utils.py
@@ -14,7 +14,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms import ToTensor
 
 ### local imports ###
-from config import *
+from AIPI540_individual_project.scripts.config import *
 
 
 ### gets the dicom files from a provided directory ###
