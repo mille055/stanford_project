166d08043d9b4e036ab1fd330c7999a6d9804e53
mille055 <mille055@gmail.com>
2023-04-06 16:35:17 -0400

    imports mlb

--- scripts/utils.py
+++ scripts/utils.py
@@ -25,6 +25,8 @@ from config import file_dict, abd_label_dict
 from config import column_lists, feats
 from config import val_list, train_val_split_percent, random_seed, data_transforms
 from config import sentence_encoder, series_description_column, text_label
+from sklearn.preprocessing import MultiLabelBinarizer
+
 
 
 ### gets the dicom files from a provided directory ###
