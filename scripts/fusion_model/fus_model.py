4838371c6f94e2ecb9bf79575e780d2bc32fcfc5
mille055 <mille055@gmail.com>
2023-04-21 17:49:01 -0400

    fixing model_container and FusionModel

--- scripts/fusion_model/fus_model.py
+++ scripts/fusion_model/fus_model.py
@@ -21,23 +21,37 @@ from model_container import ModelContainer
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
    
-
 class FusionModel(nn.Module):
-    def __init__(self, model_container, num_classes=len(classes), features=feats_to_keep, classes=classes, include_nlp=True):
+    def __init__(self, model_container, pretrained=True, num_classes=len(classes), features=feats_to_keep, classes=classes, include_nlp=True):
         super(FusionModel, self).__init__()
-        self.model_container = model_container
         self.classes = classes
         self.num_classes = num_classes
         self.features = features
         self.include_nlp = include_nlp
+        self.model_container = model_container
+    
         self.num_inputs = num_classes * 3 if self.include_nlp == True else num_classes * 2
-        
+
+        # Define the layers of the FusionModel
+        self.fusion_layer = nn.Linear(self.num_inputs, self.num_classes)
+
+        if pretrained:
+            if include_nlp:
+                weights_path = self.model_container.fusion_weights_path
+            else:
+                weights_path = self.model_container.partial_fusion_model_path
+            
+            self.load_weights(weights_path)
+
+    def load_weights(self, weights_path):
+        self.load_state_dict(torch.load(weights_path))
+
     def forward(self, x1, x2, x3=None):
         if x3 is not None:
             x = torch.cat((x1, x2, x3), dim=0)
-            x = self.model_container.fusion_model(x)
+            
         else:
             x = torch.cat((x1, x2), dim=0)
             #self.fusion_layer.weight = nn.Parameter(self.model_container.fusion_model.fusion_layer.weight)
-            #not implemented currently
+        x = self.fusion_layer(x)
         return x
--- scripts/model_container.py
+++ scripts/model_container.py
@@ -9,21 +9,14 @@ class ModelContainer:
         self.cnn_model = load_pixel_model(model_paths['cnn'])
         self.nlp_model = self.load_model(model_paths['nlp'])
         self.metadata_model = self.load_model(model_paths['meta'])
-        self.fusion_model = self.load_fusion_model(model_paths['fusion'])
-        # self.part_fusion_model = self.load_partial_fusion_model(model_paths['fusion_no_nlp'])
+        
+        # was having problems with circular calls, so just storing the path to the weights for the fusion model here
+        self.fusion_model_path = model_paths['fusion']
+        self.partial_fusion_model_path = model_paths['fusion_no_nlp']
 
     def load_model(self, model_path):
         with open(model_path, 'rb') as file:
             model = pickle.load(file)
         return model
     
-    def load_fusion_model(self, model_path):
-        with open(model_path, 'rb') as file:
-            from fusion_model.fus_model import FusionModel  #Moved here to avoid circular
-            model = pickle.load(file)
-        return model
-    ...
-    # def load_partial_fusion_model(self, model_path):
-    #      part_fusion_model = FusionModel(models, num_classes=19, include_nlp=False)
-    #      part_fusion_model.load_state_dict(torch.load(model_path))
-    #      return part_fusion_model
+    
