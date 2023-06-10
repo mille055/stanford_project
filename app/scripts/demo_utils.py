3ad4331fffbe0868e8b2e66e69a2153cde54e1d6
mille055 <mille055@gmail.com>
2023-06-10 16:30:03 -0400

    updating demo and fixing references

--- app/scripts/demo_utils.py
+++ app/scripts/demo_utils.py
@@ -23,21 +23,19 @@ def check_prediction_tag(dcm_data):
     if (0x0011, 0x1010) in dcm_data:
         prediction =  abd_label_dict[str(dcm_data[0x0011, 0x1010].value)]['short']  # this gets the numeric label written into the DICOM and converts to text description
         # if there are submodel predictions
-        prediction_meta = None
         prediction_cnn = None
-        prediction_nlp = None
+        
         if (0x0011, 0x1012) in dcm_data:
             substring = dcm_data[0x0011, 0x1012].value
             sublist = substring.split(',')
             try:
-                prediction_meta = abd_label_dict[sublist[0]]['short']
                 prediction_cnn = abd_label_dict[sublist[1]]['short']
-                prediction_nlp = abd_label_dict[sublist[2]]['short']
+                
             except Exception as e:
                 pass
-        return prediction, prediction_meta, prediction_cnn, prediction_nlp
+        return prediction_cnn
     else:
-        return None, None, None, None
+        return None
     
 
 @st.cache_resource
